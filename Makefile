4774d92872fbbbc119b97e12e7e555872e42cf6f
Mussels Ismene <mismene@er-a4.kzn.21-school.ru>
2021-07-23 17:21:30 +0300

    create socket and bind it with an address

---
+++ Makefile
@@ -0,0 +1,15 @@
+NAME = webserv
+SRC = main.cpp
+CFLAGS = -Wall -Wextra -Werror -std=c++98
+
+all: $(NAME)
+
+$(NAME): $(SRC)
+	g++ $(CFLAGS) $(SRC) -o $(NAME)
+
+clean:
+	rm $(NAME)
+
+re: clean all
+
+.PHONY: all clean re
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <iostream>
 
 int socket(int domain, int type, int protocol);
 
 int main(void) {
+	sockaddr s_addr;
+	// AF_INET - ip + port
+	// SOCK_STREAM - потоковое взаимодействие
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+		perror("error occured while socket creating");
+	if (-1 == bind(sockfd, &s_addr, sizeof(s_addr))) // did I give a size correctly?
+		perror("error occured while giving network address to a socket");
+	
 }
